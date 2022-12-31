1db6cf6459766d4e43aa69254e3043079920e855
Bayu Setra Maulana <bayumaulana472@gmail.com>
2022-12-31 18:03:56 +0700

    Multiple declare variable

--- chapter03_variable/src/main.rs
+++ chapter03_variable/src/main.rs
@@ -14,5 +14,9 @@ fn main() {
 
     number = 3;
     let message3: i8 = 24;
-    println!("Number is {1}: {0}", number, message3)
+    println!("Number is {1}: {0}", number, message3);
+
+    let (var1, var2) = (24, "Hello");
+    println!("Var1 is {0}", var1);
+    println!("Var2 is {0}", var2)
 }
