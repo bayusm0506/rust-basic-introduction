d2c7b5865d3e89437011ccf7ef8ed378ff650efd
Bayu Setra Maulana <bayumaulana472@gmail.com>
2022-12-31 17:36:23 +0700

    Variable declare using of let

---
+++ chapter03_variable/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // {} : string literal
+
+    let variable_name = "Predefine Value";
+    println!("{}", variable_name);
+
+    let number = 1;
+    let message = "Hello";
+    println!("Number is {}: {}", number, message);
+}
