fa706748861766a7f3eac751f50ce715e948f109
Bayu Setra Maulana <bayumaulana472@gmail.com>
2023-01-01 16:27:32 +0700

    Unsigned integer data type

--- chapter04_primitive_scalar_data_type/src/main.rs
+++ chapter04_primitive_scalar_data_type/src/main.rs
@@ -3,4 +3,9 @@ fn main() {
     let numeric2: i8 = 127;
     let numeric3: i64 = 12;
     println!("{} | {} | {}", numeric1, numeric2, numeric3);
+
+    let numeric4: u32 = 28;
+    let numeric5: u8 = 16;
+    let numeric6: u64 = 42;
+    println!("{} | {} | {}", numeric4, numeric5, numeric6);
 }
