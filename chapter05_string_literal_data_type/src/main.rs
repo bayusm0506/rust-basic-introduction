81cef8bc428b175e5e9ac231e1d684c519278b47
Bayu Setra Maulana <bayumaulana472@gmail.com>
2023-01-04 22:30:02 +0700

    String literal use escape character

--- chapter05_string_literal_data_type/src/main.rs
+++ chapter05_string_literal_data_type/src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
     let var1= "Hello";
-    println!("{}", var1)
+    println!("{}", var1);
+
+    let var2 = "Hello \
+        \"Jhon\" \
+        and \
+        \"Doe\"";
+    println!("{}", var2);
+
 }
