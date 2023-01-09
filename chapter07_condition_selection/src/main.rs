7a7aa165d83af1a6015ceeffe1dff27731bee654
Bayu Setra Maulana <bayumaulana472@gmail.com>
2023-01-09 21:37:51 +0700

    Keyword if

---
+++ chapter07_condition_selection/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let numb_a = 3;
+    if numb_a < 5 {
+        println!("Number a is left than 5");
+    }
+
+    let result_a = numb_a >= 5;
+    if result_a {
+        println!("Result a is more than or equals 5");
+    }
+}
