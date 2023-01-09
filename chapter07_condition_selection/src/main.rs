f65f5c1294740ba851172506560147dc7dd5d0fc
Bayu Setra Maulana <bayumaulana472@gmail.com>
2023-01-09 21:41:49 +0700

    Keyword if, else if & else

--- chapter07_condition_selection/src/main.rs
+++ chapter07_condition_selection/src/main.rs
@@ -8,4 +8,13 @@ fn main() {
     if result_a {
         println!("Result a is more than or equals 5");
     }
+
+    let numb_b = 3;
+    if numb_b == 2 {
+        println!("Number b is 2");
+    } else if numb_b < 2 {
+        println!("Number b is left than 2");
+    } else {
+        println!("Number b is more than 2");
+    }
 }
