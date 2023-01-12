72ff6e57b028dee1fd888b86e37810a7ebd6e059
Bayu Setra Maulana <bayumaulana472@gmail.com>
2023-01-12 22:04:35 +0700

    Nested while

--- chapter08_while_loop/src/main.rs
+++ chapter08_while_loop/src/main.rs
@@ -6,4 +6,20 @@ fn main() {
         println!("Value: {i}");
         i += 1;
     }
+
+    let mut j= 0;
+    let maximum = 5;
+
+    while j < maximum {
+        let mut k = 0;
+        let max_inner = j;
+
+        while k <= max_inner {
+            print!("* ");
+            k += 1;
+        }
+
+        println!();
+        j += 1;
+    }
 }
