7fa3260b68caf62bb61f3dd944f050330e437fcc
Bayu Setra Maulana <bayumaulana472@gmail.com>
2023-01-21 23:54:49 +0700

    Label loop

--- chapter10_for_in_loop/src/main.rs
+++ chapter10_for_in_loop/src/main.rs
@@ -2,4 +2,11 @@ fn main() {
     for i in 0..5 {
         println!("{i}");
     }
+
+    'perulangan: for j in 0..=5 {
+        if j > 3 {
+            println!("stop loop to iteration {j}");
+            break 'perulangan;
+        }
+    }
 }
