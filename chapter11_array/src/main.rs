e03af9762a982d118ddc0fb20d80e77650b2f731
Bayu Setra Maulana <bayumaulana472@gmail.com>
2023-01-25 23:16:19 +0700

    Array declaration use type inference

--- chapter11_array/src/main.rs
+++ chapter11_array/src/main.rs
@@ -11,5 +11,11 @@ fn main() {
 
     numbers[1] = 16;
     numbers[3] = 8;
-    println!("Array {numbers:?}")
+    println!("Array {numbers:?}");
+
+    let integer_numbers = [24, 17, 32, 12];
+    println!("{integer_numbers:?}");
+
+    let float_numbers = [24.1, 17.2, 32.3, 12.4];
+    println!("{float_numbers:?}");
 }
