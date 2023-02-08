8beff483e431a2e3d024e1d81573a2f589121559
Bayu Setra Maulana <bayumaulana472@gmail.com>
2023-02-08 22:40:06 +0700

    Mutable tuple

--- chapter13_tuple/src/main.rs
+++ chapter13_tuple/src/main.rs
@@ -6,4 +6,12 @@ fn main() {
     println!("index 1: {:?}", tuple_a.1);
     println!("index 2: {:?} {:?}", tuple_a.2[0], tuple_a.2[1]);
     println!("index 3: {:?}", tuple_a.3);
+
+    let mut tuple_b: (&str, i32, [&str; 2], bool) = ("default", 0, [""; 2], false);
+    tuple_b.0 = "B";
+    tuple_b.1 = 2;
+    tuple_b.2 = ["Gaming", "Adventuring"];
+    tuple_b.3 = true;
+
+    println!("tuple_b: {:?}", tuple_b);
 }
