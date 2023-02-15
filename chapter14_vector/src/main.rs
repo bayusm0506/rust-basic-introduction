abc787e32c0487af93df72b3aa9ef778fd65456a
Bayu Setra Maulana <bayumaulana472@gmail.com>
2023-02-15 21:42:16 +0700

    Update index element

--- chapter14_vector/src/main.rs
+++ chapter14_vector/src/main.rs
@@ -4,12 +4,14 @@ fn main() {
 
     data_one.pop();
     data_one.push("constantine");
+    data_one[2] = "red hood";
     println!("data: {:?}", data_one);
     println!("length: {}, capacity: {}", data_one.len(), data_one.capacity());
 
     data_two.remove(1);
     data_two.push("trigon");
     data_two.push("darkseid");
+    data_two[2] = "black hood";
     println!("data two: {:?}", data_two);
     println!("length two: {}, capacity two: {}", data_two.len(), data_two.capacity());
 }
