b7fc397717aa02cc4f523a5057e5f75cc34c2fc3
Bayu Setra Maulana <bayumaulana472@gmail.com>
2023-02-15 21:36:24 +0700

    Remove specific element

--- chapter14_vector/src/main.rs
+++ chapter14_vector/src/main.rs
@@ -1,8 +1,12 @@
 fn main() {
     let mut data_one = vec!["batman", "superman", "lobo"];
+    let mut data_two = vec!["ninja-saga", "thor", "strange"];
 
     data_one.pop();
-
     println!("data: {:?}", data_one);
-    println!("length: {}, capacity: {}", data_one.len(), data_one.capacity())
+    println!("length: {}, capacity: {}", data_one.len(), data_one.capacity());
+
+    data_two.remove(1);
+    println!("data two: {:?}", data_two);
+    println!("length two: {}, capacity two: {}", data_two.len(), data_two.capacity());
 }
