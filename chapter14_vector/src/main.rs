d42a91bbca78df418fd87a003f7ab0de288753cf
Bayu Setra Maulana <bayumaulana472@gmail.com>
2023-02-15 21:29:27 +0700

    Vector, size & capacity declaration

---
+++ chapter14_vector/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let data_one = vec!["batman", "superman", "lobo"];
+
+    println!("data: {:?}", data_one);
+    println!("length: {}, capacity: {}", data_one.len(), data_one.capacity())
+}
