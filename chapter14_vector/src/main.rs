0c8afff566aba87c3785272bcda77a6fe841fd91
Bayu Setra Maulana <bayumaulana472@gmail.com>
2023-02-15 21:39:00 +0700

    Add new element

--- chapter14_vector/src/main.rs
+++ chapter14_vector/src/main.rs
@@ -3,10 +3,13 @@ fn main() {
     let mut data_two = vec!["ninja-saga", "thor", "strange"];
 
     data_one.pop();
+    data_one.push("constantine");
     println!("data: {:?}", data_one);
     println!("length: {}, capacity: {}", data_one.len(), data_one.capacity());
 
     data_two.remove(1);
+    data_two.push("trigon");
+    data_two.push("darkseid");
     println!("data two: {:?}", data_two);
     println!("length two: {}, capacity two: {}", data_two.len(), data_two.capacity());
 }
